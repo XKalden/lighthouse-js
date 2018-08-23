22edd33ed6a61d29f8aae6a1c5ef38d3184885aa
XKalden <kaldend.tsering@outlook.com>
2018-08-22 20:48:48 -0400

    final

---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+function math(first, seconds, third){
+
+    return first + seconds * third;
+
+}
+
+console.log(math(53,61,67));
---
+++ test.js
@@ -0,0 +1,13 @@
+function concat(arr1, arr2) {
+
+    var arr3 = arr1.concat(arr2);
+    console.log(arr3);
+
+}
+
+
+
+
+concat([ 1, 2, 3 ], [ 4, 5, 6 ]);
+
+
